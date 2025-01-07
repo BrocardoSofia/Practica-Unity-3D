0f2729d3e105ee9a78b20d5465f259eec5e6bbca
BrocardoSofia <sofia.brocardo@hotmail.com>
2025-01-07 19:50:53 -0300

    Creating Collectable Coins

---
+++ Simple 3D Game/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 0, rotateSpeed);
+
+    
+    }
+}
