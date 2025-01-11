9ff8123f5d473eb30d00ea88fceb819a972b2aad
BrocardoSofia <sofia.brocardo@hotmail.com>
2025-01-10 23:01:33 -0300

    Input Manager Code

---
+++ FPS Game/Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
