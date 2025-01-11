1adc63401c400ab8989c98cd9ddcb8129c979553
BrocardoSofia <sofia.brocardo@hotmail.com>
2025-01-10 23:12:36 -0300

    Movement Code

---
+++ FPS Game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        moveDirection.x = input.x;
+        moveDirection.y = input.y;
+        controller.Move(transform.TransformDirection(
+            moveDirection) * speed * Time.deltaTime);
+    }
+}
