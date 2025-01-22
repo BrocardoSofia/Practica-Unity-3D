e00fbc447249e5ed291e492ec049e36ff4d55a32
BrocardoSofia <sofia.brocardo@hotmail.com>
2025-01-21 21:19:35 -0300

    Interaction events

--- FPS Game/Assets/Scripts/Interactable.cs
+++ FPS Game/Assets/Scripts/Interactable.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    public bool useEvents;
+    [SerializeField]
     public string prompMessage;
+
+    public virtual string OnLook()
+    {
+        return prompMessage;
+    }
 
     public void BaseInteract()
     {
+        if(useEvents)
+        {
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
+        }
         Interact();
     }
     protected virtual void Interact()
